2be1283a29d42ede7bcf75fb8b3250b62285ab91
Petabyte <brikbusters@gmail.com>
2020-07-06 17:49:57 -0400

    Update build.sh

--- template/build.sh
+++ template/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo Building Go...
+
 # Link this to your repo
-gomobile bind -o app/webview.aar github.com/pufflegamerz/webview
+gomobile bind -o app/webview.aar github.com/pufflegamerz/gowebviewex
+
 echo Done, now building APK...
 ./gradlew build
+
 # Add any other things you would like to run after building
 # (like adb installing the apk)
